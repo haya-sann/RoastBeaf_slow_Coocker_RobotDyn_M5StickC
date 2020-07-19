4cf39db62fea3250df93d41923d1dd0136ac78bd
Nobuo Hayashi <haya.biz@gmail.com>
2020-07-19 20:47:04 +0900

    Update blynkToken.h

--- blynkToken.h
+++ blynkToken.h
@@ -1,4 +1,4 @@
 //// You should get Auth Token in the Blynk App.
 //// Go to the Project Settings (nut icon).
 //// This is a secret key for BLYNK
-char auth[]="cz5gjVi4ps-Uci3usFJ04t9Q4wDj4xSn"; //Auth Token:低温調理器用
+char auth[]="Your Blynk auth key"; //Auth Token:低温調理器用
